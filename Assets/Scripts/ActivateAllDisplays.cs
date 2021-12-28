0bb59dff18c9276ff6d4d3bdb9ab301bd1c6a226
Editscode <38367082+Editscode@users.noreply.github.com>
2021-12-28 17:01:00 +0300

    Text and script
    Обновил скрипт активации экранов
    Обновил размеры
    Фикс текста

--- Assets/Scripts/ActivateAllDisplays.cs
+++ Assets/Scripts/ActivateAllDisplays.cs
@@ -6,8 +6,6 @@ public class ActivateAllDisplays : MonoBehaviour
     void Start()
     {
         Debug.Log("displays connected: " + Display.displays.Length);
-        // Display.displays[0] is the primary, default display and is always ON, so start at index 1.
-        // Check if additional displays are available and activate each.
 
         for (int i = 1; i < Display.displays.Length; i++)
         {
@@ -15,8 +13,4 @@ public class ActivateAllDisplays : MonoBehaviour
         }
     }
 
-    void Update()
-    {
-
-    }
 }
