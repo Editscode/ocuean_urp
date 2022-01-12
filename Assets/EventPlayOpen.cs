54c9c1d03a25e1b044bdb4ebb420bf32aa2238bf
Editscode <38367082+Editscode@users.noreply.github.com>
2022-01-12 15:10:04 +1000

    animation
    Приближение камеры и отдаление
    Открытие
    новые бар
    новые png

---
+++ Assets/EventPlayOpen.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventPlayOpen : MonoBehaviour
+{
+    [SerializeField]
+    private Animator m_AnimatorControllerModel;
+    [SerializeField]
+    private Animator m_AnimatorControllerCam;
+    private bool active = false;
+
+
+
+    public void ActiveButton()
+    {
+        if (active)
+        {
+            CloseEvent();
+            active = false;
+        }
+        else
+        {
+            OpenEvent();
+            active = true;
+        }
+    }
+    private void OpenEvent()
+    {
+        m_AnimatorControllerCam.Play("Move");
+        m_AnimatorControllerModel.Play("Open");
+       
+    }
+
+    private void CloseEvent()
+    {
+        m_AnimatorControllerCam.Play("BackMove");
+        m_AnimatorControllerModel.Play("Close");
+
+    }
+}
